77b3104252f0ae1c1b43d94e6eea0c9b6ec4d9cb
Deivison Cardoso <deivisondc@gmail.com>
2020-08-14 15:32:04 -0300

    add classes routes

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import routes from './routes';
 
 const app = express();
 
 app.use(cors())
 app.use(express.json());
-
-app.get('/users', (req, res) => {
-  return res.json({ teste: 123 })
-});
+app.use(routes);
 
 app.listen(3333);
